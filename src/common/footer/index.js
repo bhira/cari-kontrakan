930b0842d85993c49acf990c92b7e47cec1bfdb7
adiatma9024 <adiatma9024@gmail.com>
2019-04-03 11:27:26 +0700

    update: cari kontrakan

--- src/common/footer/index.js
+++ src/common/footer/index.js
@@ -4,13 +4,21 @@ import Container from 'ui/container'
 import Columns from 'ui/columns'
 import Paragraph from 'ui/paragraph'
 
-export default function Footer(props) {
+export default function Footer() {
   return (
     <StyledDiv>
       <Container>
-        <Columns>
+        <Columns templateColumn={3}>
           <Columns.Item>
             <Paragraph fontWeight={`500`}>Tentang Kami</Paragraph>
+            <Paragraph>Lorem ipsume sit amet dolor</Paragraph>
+          </Columns.Item>
+          <Columns.Item>
+            <Paragraph fontWeight={`5000`}>Site Map</Paragraph>
+            <Paragraph>Lorem ipsume sit amet dolor</Paragraph>
+          </Columns.Item>
+          <Columns.Item>
+            <Paragraph fontWeight={`5000`}>Categories</Paragraph>
             <Paragraph>Lorem ipsume sit amet dolor</Paragraph>
           </Columns.Item>
         </Columns>
